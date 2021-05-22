64314406b37fa1017cf59148460f578927737314
Gobal Krishnan V <64263677+engineer-ece@users.noreply.github.com>
2021-05-22 20:27:54 +0530

    Add files via upload

--- tkinter/place.py
+++ tkinter/place.py
@@ -9,19 +9,23 @@ from tkinter import *
 
 root = Tk()
 root.title("place")
-root.geometry("200x60")
+root.geometry("210x80")
 a = StringVar()
 
-l = Entry(root,textvariable=a,font=(20))
-l.place(x=0,y=0)
+sb = Scrollbar(root,orient=HORIZONTAL,)
+sb.pack(fill=X,expand=True)
 
+
+l = Entry(root,textvariable=a,font=(20),xscrollcommand=sb.set)
+l.place(x=0,y=0,bordermode=INSIDE)
+sb.config(command=l.xview)
 def cal():
     ev = eval(a.get())
     a.set(ev)
     print(ev)
 
 b = Button(root,text="calculate",width=20,command=cal)
-b.place(x=0,y=30)
+b.place(x=0,y=50)
 
 print((root.place_slaves()))
 root.mainloop()
